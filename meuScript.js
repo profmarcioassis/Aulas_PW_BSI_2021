1188d72af6607284869e298e374962148b174fba
Prof. Márcio Assis <assismiranda@gmail.com>
2021-12-06 20:20:50 -0300

    update

---
+++ meuScript.js
@@ -0,0 +1,9 @@
+function retornaDisciplina(){
+    let disciplina = "Programação Web";
+    return disciplina;
+    //document.getElementById("demo").innerHTML = disciplina;
+}
+
+function Soma(a, b){
+    return a + b;
+}
