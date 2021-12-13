afcdc0c7b2bfe1e88c48899fe9417a195ce899a9
Prof. Márcio Assis <assismiranda@gmail.com>
2021-12-13 20:37:48 -0300

    update

---
+++ php/aula01.php
@@ -0,0 +1,20 @@
+<?php
+$nome_professor = "Márcio Assis Miranda";
+$valor = 10.0;
+
+echo "Meu primeiro script php";
+$x = 10;
+if ($x == 10) {
+    echo "se for igual a 10";
+}
+?>
+
+<br>
+
+<?php
+echo 'O nome do professor é '. $nome_professor;
+echo "<br>O valor de x é $x";
+echo '<br>O valor de x é $x';
+echo '<br>O valor de x é ' . $x;
+//. é o operador de concatenação
+?>
